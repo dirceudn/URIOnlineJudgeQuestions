158e47e86432e14328dc6efb9aba4a923c082639
U-DESKTOP-9P6N1N9\dirceu <dircco@bitbucket.org>
2016-01-09 18:39:34 -0200

    Adicionando primeiro problema

---
+++ Problema1001.cpp
@@ -0,0 +1,21 @@
+// URIJudge-Questions.cpp : Defines the entry point for the console application.
+//
+
+#include "stdafx.h"
+#include <iostream>
+#include <conio.h>
+#include <cstdlib>
+using namespace std;
+
+
+int main()
+{
+
+	int A, B,X;
+	cin >> A;
+	cin >> B;
+	X = A + B;
+	cout << "X = " << X << endl;    
+	return 0;
+}
+
