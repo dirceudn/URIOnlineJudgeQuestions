11b03d4099d1f416b89aba29121c91363d461106
U-DESKTOP-9P6N1N9\dirceu <dircco@bitbucket.org>
2016-01-09 23:06:43 -0200

    Problema 1003

---
+++ Problema1003.cpp
@@ -0,0 +1,20 @@
+// URIJudge-Questions.cpp : Defines the entry point for the console application.
+//
+
+#include "stdafx.h"
+#include <iostream>
+#include <iomanip>
+using namespace std;
+
+
+int main()
+{
+
+	int A, B, sum;
+	cin >> A;
+	cin >> B;
+	sum = A + B;	
+    cout << "SOMA = " << sum << endl;
+	return 0;
+}
+
